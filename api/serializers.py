d2ba4a58a3adc4c6cddc3a8f083213755e41cb77
rhlklwr <kalwar.rahul@gmail.com>
2021-03-13 20:19:46 +0530

    components created, Room logic created

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,8 @@
 from django.db.models import fields
 from rest_framework import serializers
 from .models import Room
+
+
 class RoomSerializer(serializers.ModelSerializer):
     class Meta:
         model = Room
@@ -8,3 +10,9 @@ class RoomSerializer(serializers.ModelSerializer):
             'id', 'code', 'host', 'guest_can_pause',
             'votes_to_skip', 'created_at',
             )
+
+
+class CreateRoomSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Room
+        fields = ('guest_can_pause', 'votes_to_skip')
