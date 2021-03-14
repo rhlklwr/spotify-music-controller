e68188257d5b47123fcb6a158adbe61496d9728f
rhlklwr <kalwar.rahul@gmail.com>
2021-03-14 17:18:27 +0530

    Checkpoint

--- api/views.py
+++ api/views.py
@@ -1,9 +1,11 @@
+from django.http.response import JsonResponse
 from django.shortcuts import render
 from rest_framework import generics, serializers, status
 from .serializers import RoomSerializer, CreateRoomSerializer
 from .models import Room
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from django.http import JsonResponse
 
 
 # Create your views here.
@@ -79,3 +81,14 @@ class CreateRoomView(APIView):
 
 
         return Response({'Bad Request': 'Invalid data...'}, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UserInRoom(APIView):
+    def get(self, request, format=None):
+        if not (self.request.session.exists(self.request.session.session_key)):
+            self.request.session.create()
+            
+        data = {
+            'code': self.request.session.get('room_code'),
+        }
+        return JsonResponse(data, status=status.HTTP_200_OK)
