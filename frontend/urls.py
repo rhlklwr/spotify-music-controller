943bf92052d3c939db59e58bcd098a91cf2ed33c
rhlklwr <kalwar.rahul@gmail.com>
2021-03-16 10:01:22 +0530

    connected to spotify api

--- frontend/urls.py
+++ frontend/urls.py
@@ -2,8 +2,10 @@
 from django.urls import path
 from . import views
 
+app_name = 'frontend'
+
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name=''),
     path('join', views.index),
     path('create', views.index),
     path('room/<str:roomCode>', views.index),
