449e7ba6f276884822807e015d86a2972b174c4c
rhlklwr <kalwar.rahul@gmail.com>
2021-03-16 14:54:21 +0530

    Ready for deploy

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -127,10 +127,3 @@ STATIC_ROOT = Path(BASE_DIR).joinpath('staticfiles')
 
 STATIC_URL = '/static/'
 
-MEDIA_URL = '/images/'
-
-STATICFILES_DIRS = [
-    Path(BASE_DIR).joinpath('static')
-]
-
-MEDIA_ROOT = Path(BASE_DIR).joinpath('static/images')
