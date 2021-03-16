d1fae6ddd422d4b5d6e0be0ae3adf70df3aa7026
rhlklwr <kalwar.rahul@gmail.com>
2021-03-16 14:50:49 +0530

    Ready for deploy

--- music_controller/settings.py
+++ music_controller/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -122,4 +123,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = Path(BASE_DIR).joinpath('staticfiles')
+
 STATIC_URL = '/static/'
+
+MEDIA_URL = '/images/'
+
+STATICFILES_DIRS = [
+    Path(BASE_DIR).joinpath('static')
+]
+
+MEDIA_ROOT = Path(BASE_DIR).joinpath('static/images')
